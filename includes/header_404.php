21a51364cb3773566d2a031014582daae26cabb8
denver1981denver <84741346+denver1981denver@users.noreply.github.com>
2021-10-29 21:11:34 +0300

    Add files via upload

--- includes/header_404.php
+++ includes/header_404.php
@@ -1,9 +1,5 @@
-<?php
-include_once "includes/functions.php";
-	$users_count = get_user_count();
-	$views_count = get_views_count();
-	$links_count = get_links_count();
-	
+<?php 
+	include_once "includes/functions.php";
 ?>
 <!doctype html>
 <html lang="ru">
@@ -13,10 +9,9 @@ include_once "includes/functions.php";
 				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
-	<title><?php echo SITE_NAME; ?></title>
+	<title><?php echo SITE_NAME; ?> - 404 Not found</title>
 </head>
 <body>
-
 	<header>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div class="container">
@@ -27,27 +22,15 @@ include_once "includes/functions.php";
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 						<li class="nav-item">
-							<a class="nav-link " aria-current="page" href="<?php echo get_url(); ?>">Главная</a>
+							<a class="nav-link active" aria-current="page" href="<?php echo get_url(); ?>">Главная</a>
 						</li>
-							<?php if (isset($_SESSION['user']['id']) && !empty($_SESSION['user']['id'])) { ?>
-							<li class="nav-item">
-							<a class="nav-link active" href="<?php echo get_url("profile.php"); ?>">Профиль</a>
-						</li>
-						<?php } ?>
 					</ul>
 					<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 						<li class="nav-item">
-							<?php if (isset($_SESSION['user']['id']) && !empty($_SESSION['user']['id'])) { ?>
-							<a href="<?php echo get_url("includes/logout.php"); ?>" class="btn btn-primary">Выйти</a>
-							<?php } else { ?>
-							<a href="<?php echo get_url('login.php'); ?>" class="btn btn-primary"> Войти</a>
-							<?php } ?>
+							<a href="<?php echo get_url('login.php'); ?>" class="btn btn-primary">Войти</a>
 						</li>
 					</ul>
 				</div>
 			</div>
 		</nav>
 	</header>
-	<pre>
-	<?php var_dump($_SESSION); ?>
-							</pre>
